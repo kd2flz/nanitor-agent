7e33c309e1940c441ab3ba844a82d3b665300ed6
David Rhoads <davidrhoads@mailstack.com>
2025-12-31 13:17:34 -0500

    Revert "Remove ExecStartPre and move mkdir -p and chown commands #3"
    This reverts commit 83e28c3a36b3598175c256588484e202196ecf7c.

--- modules/services/nanitor-agent.nix
+++ modules/services/nanitor-agent.nix
@@ -133,6 +133,7 @@ in
         );
 
         WorkingDirectory = "${cfg.dataDir}/agent";
+        ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p /var/log/nanitor && ${pkgs.coreutils}/bin/chown ${cfg.user}:${cfg.group} /var/log/nanitor";
         ExecReload = "${pkgs.coreutils}/bin/kill -HUP $MAINPID";
         Restart = "on-failure";
         RestartSec = "42s";
@@ -160,14 +161,10 @@ in
         lib.mkIf cfg.enroll.enable ''
           set -euo pipefail
 
-          # Ensure /var/log/nanitor exists and has correct ownership for file logging.
-          ${pkgs.coreutils}/bin/mkdir -p /var/log/nanitor
-          ${pkgs.coreutils}/bin/chown ${cfg.user}:${cfg.group} /var/log/nanitor
-
           ${serverUrlScript}
 
-           # If not enrolled, run signup
-           if ! ${bin} is-signedup >/dev/null 2>&1; then
+          # If not enrolled, run signup
+          if ! ${bin} is-signedup >/dev/null 2>&1; then
             echo "[nanitor-agent unit] Not enrolled yet; attempting signup"
             ${bin} signup || echo "[nanitor-agent unit] signup failed; agent may not connect"
           else
